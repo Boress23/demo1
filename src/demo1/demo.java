11d5f5ea84050e550c5e6b3f75c7d36b5cb499e6
Boress23 <92275270+Boress23@users.noreply.github.com>
2023-02-13 20:32:16 +0100

    fin segunda parte

--- src/demo1/demo.java
+++ src/demo1/demo.java
@@ -7,7 +7,7 @@ public class demo {
 		System.out.println("Hello world!");
 		System.out.println("Hello world2!");
 		System.out.println("Hello world3!");
-		System.out.println("A ver si genera SOL.");
+		System.out.println("A ver si genera SOL. ");
 		// Esto es la parte 6.1
 	}
 
